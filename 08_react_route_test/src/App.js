985694f0718ae59f1f316b7e0b86be9f69fe2f55
Abed <abbe_92@live.com>
2023-10-04 14:56:50 +0300

    main

--- 08_react_route_test/src/App.js
+++ 08_react_route_test/src/App.js
@@ -3,24 +3,24 @@ import './App.css';
 import Home from './components/Home';
 import Secret from './components/Secret';
 import About from './components/About';
-import { Route, Routes, Link, Navigate } from 'react-router-dom';
+import {Route,Routes,Link,Navigate} from 'react-router-dom';
 
 function App() {
-	return (
-		<div className="App">
-			<ul style={{ listStyleType: "none" }}>
-				<li><Link to="/">Home</Link></li>
-				<li><Link to="/about">About</Link></li>
-			</ul>
-			<hr />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/secret" element={<Secret />} />
-				<Route path="*" element={<Navigate to="/" />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+		<ul style={{listStyleType:"none"}}>
+			<li><Link to="/">Home</Link></li>
+			<li><Link to="/about">About</Link></li>
+		</ul>
+		<hr/>
+		<Routes>
+			<Route path="/" element={<Home/>}/>
+			<Route path="/about" element={<About/>}/>
+			<Route path="/secret" element={<Secret/>}/>
+			<Route path="*" element={<Navigate to="/" />}/>
+		</Routes>
+    </div>
+  );
 }
 
 export default App;
