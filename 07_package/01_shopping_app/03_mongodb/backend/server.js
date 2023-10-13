47cc364287eaf5d2c45faf91624721bbee000982
Abed <abbe_92@live.com>
2023-10-13 03:01:38 +0300

    .

--- 07_package/01_shopping_app/03_mongodb/backend/server.js
+++ 07_package/01_shopping_app/03_mongodb/backend/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const shoppingroute = require("./routes/shoppingroute");
 const crypto = require("crypto");
 const bcrypt = require("bcrypt");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const userModel = require("./models/user");
+const sessionModel = require("./models/session");
+require('dotenv').config();
 
 let app = express();
 
@@ -19,18 +21,17 @@ const mongo_url = process.env.MONGODB_URL;
 const mongo_user = process.env.MONGODB_USER;
 const mongo_password = process.env.MONGODB_PASSWORD;
 
-//const url = "mongodb+srv://"+mongo_user+":"+mongo_password+"@"+mongo_url+"/?retryWrites=true&w=majority"
-const url = "mongodb+srv://DAX:TQp5tFoHR7qENVPd@testcluster.ebmengm.mongodb.net/?retryWrites=true&w=majority"
+const url = "mongodb+srv://"+mongo_user+":"+mongo_password+"@"+mongo_url+"/shoppingdatabase?retryWrites=true&w=majority"
+
 
 mongoose.connect(url).then(
 () => console.log("Connected to Mongo Atlas"),
 (error) => console.log("Failed to connect to Mongo Atlas. Reason",error)
 )
 
-//LOGIN DATABASES
+mongoose.set("toJSON",{virtuals:true});
 
-let registeredUsers = [];
-let loggedSessions = [];
+//LOGIN DATABASES
 const time_to_live_diff = 3600000;
 
 //LOGIN MIDDLEWARES
@@ -44,21 +45,33 @@ const isUserLogged = (req,res,next) => {
 	if(!req.headers.token) {
 		return res.status(403).json({"Message":"Forbidden"});
 	}
-	for(let i=0;i<loggedSessions.length;i++) {
-		if(req.headers.token === loggedSessions[i].token) {
-			let now = Date.now();
-			if(now > loggedSessions[i].ttl) {
-				loggedSessions.splice(i,1);
-				return res.status(403).json({"Message":"Forbidden"});
-			} else {
-				loggedSessions[i].ttl = now+time_to_live_diff;
-				req.session = {};
-				req.session.user = loggedSessions[i].user;
+	sessionModel.findOne({"token":req.headers.token}).then(function(session) {
+		if(!session) {
+			return res.status(403).json({"Message":"Forbidden"});
+		}
+		let now = Date.now();
+		if(now > session.ttl) {
+			sessionModel.deleteOne({"_id":session._id}).then(function() {
+				return res.status(403).json({"Message":"Forbidden"})
+			}).catch(function(error) {
+				console.log("Failed to remove session. Reason",error);
+				return res.status(403).json({"Message":"Forbidden"})
+			})
+		} else {
+			session.ttl = now + time_to_live_diff;
+			req.session = {};
+			req.session.user = session.user;
+			session.save().then(function() {
 				return next();
-			}
-		} 
-	}
-	return res.status(403).json({"Message":"Forbidden"});
+			}).catch(function(error) {
+				console.log("Failed to resave session. Reason",error);
+				return next();
+			})
+		}
+	}).catch(function(error){
+		console.log("Failed to find session. Reason",error);
+		return res.status(403).json({"Message":"Forbidden"})
+	})
 }
 
 //LOGIN API
@@ -73,17 +86,12 @@ app.post("/register",function(req,res) {
 	if(req.body.username.length < 4 || req.body.password.length < 8) {
 		return res.status(400).json({"Message":"Bad Request"});
 	}
-	for(let i=0;i<registeredUsers.length;i++) {
-		if(req.body.username === registeredUsers[i].username) {
-			return res.status(409).json({"Message":"Username already in use"});
-		}
-	}
 	bcrypt.hash(req.body.password,14,function(err,hash) {
 		if(err) {
 			console.log(err);
 			return res.status(500).json({"Message":"Internal server error"})
 		}
-		let user = new userModel ({
+		let user = new userModel({
 			username:req.body.username,
 			password:hash
 		})
@@ -108,43 +116,48 @@ app.post("/login",function(req,res) {
 	if(req.body.username.length < 4 || req.body.password.length < 8) {
 		return res.status(400).json({"Message":"Bad Request"});
 	}
-	for(let i=0;i<registeredUsers.length;i++) {
-		if(req.body.username === registeredUsers[i].username) {
-			bcrypt.compare(req.body.password,registeredUsers[i].password,function(err,success) {
-				if(err) {
-					console.log(err);
-					return res.status(500).json({"Message":"Internal server error"});
-				}
-				if(!success) {
-					return res.status(401).json({"Message":"Unauthorized"});
-				}
-				let token = createToken();
-				let now = Date.now();
-				let session = {
-					"token":token,
-					"user":req.body.username,
-					"ttl":now+time_to_live_diff
-				}
-				loggedSessions.push(session);
-				return res.status(200).json({"token":token});
-			})
-			return;
+	userModel.findOne({"username":req.body.username}).then(function(user) {
+		if(!user) {
+			return res.status(401).json({"Message":"Unauthorized"});
 		}
-	}
-	return res.status(401).json({"Message":"Unauthorized"});
+		bcrypt.compare(req.body.password,user.password,function(err,success) {
+			if(err) {
+				console.log(err);
+				return res.status(500).json({"Message":"Internal server error"});
+			}
+			if(!success) {
+				return res.status(401).json({"Message":"Unauthorized"});
+			}
+			let token = createToken();
+			let now = Date.now();
+			let session = new sessionModel({
+				"token":token,
+				"user":req.body.username,
+				"ttl":now+time_to_live_diff
+			})
+			session.save().then(function() {
+				return res.status(200).json({"token":token});
+			}).catch(function(error) {
+				console.log(error);
+				return res.status(500).json({"Message":"Internal Server Error"});
+			});
+		})
+	}).catch(function(error) {
+		console.log(error);
+		return res.status(500).json({"Message":"Internal Server Error"})
+	})
 })
 
 app.post("/logout",function(req,res) {
 	if(!req.headers.token) {
 		return res.status(404).json({"Message":"Not found"});
 	}
-	for(let i=0;i<loggedSessions.length;i++) {
-		if(req.headers.token === loggedSessions[i].token) {
-			loggedSessions.splice(i,1);
-			return res.status(200).json({"Message":"Logged out"});
-		}
-	}
-	return res.status(404).json({"Message":"Not found"});
+	sessionModel.deleteOne({"token":req.headers.token}).then(function() {
+		return res.status(200).json({"Message":"Logged out"})
+	}).catch(function(error) {
+		console.log("Failed to remove session in logout. Reason",error);
+		return res.status(500).json({"Message":"Internal Server Error"});
+	})
 })
 
 app.use("/api",isUserLogged,shoppingroute);
