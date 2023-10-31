f842c3835497651639eb380e804652d5db3dedd6
Abed <abbe_92@live.com>
2023-10-31 05:14:50 +0200

    Untrack the __pycache__ folder

--- 07_package/01_shopping_app/05_stage_statemanager/frontend/src/context/ActionContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ActionContext = React.createContext();
-
-ActionContext.displayName = "ActionContext";
-
-export default ActionContext;
--- 13_number_guess_game/src/context/GameContect.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const GameContext = React.createContext();
-
-GameContext.displayName = "GameContext"
-
-export default GameContext;
